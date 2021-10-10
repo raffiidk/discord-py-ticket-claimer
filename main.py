350aa42206a65f55c4c159b911548716bba0b9ff
raffiidk <70536924+raffiidk@users.noreply.github.com>
2021-10-11 00:24:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import oauth2client
 import os
 import time
 import json
@@ -7,10 +8,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = ['https://spreadsheets.google.com/feeds']
 scope = ['https://spreadsheets.google.com/feeds',
         'https://www.googleapis.com/auth/drive']
-token = "NjUyMDY4NzE0MDU3MDM5ODcz.YHCJvg.72QmL95tfejmB1wXBeYR0rJpFqY"
-creds = ServiceAccountCredentials.from_json_keyfile_name("yourtoken.json",scope)
-client = gspread.authorize(creds)
-sheet = client.open("Keys").sheet1
+token = "</>"
+#
 intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
@@ -27,10 +26,11 @@ def write(data, filename):
         json.dump(data,f,indent=4)
         f.close()
 
-file = "settings.json"
+file = input("File:")
+
 
 def load():
-    with open("settings.json","r") as f:
+    with open(file,"r") as f:
         data = json.load(f)
         print("Loading...")
         data2 = data["settings"]
@@ -45,27 +45,10 @@ def load():
         trigger = 0
         f.close()
         return delay,token,message,serverid,keyword,access
-        #Settings.json is required in parent dir
 delay2,token,message,serverid,keyword,access = load()
 serverid = int(serverid)
 delay2 = float(delay2)
-@client.event
-async def on_ready():
-    time.sleep(2)
-    keys = sheet.col_values(1)
-    print(keys)
-    if access in keys:
-        print('''Self-bot auto ticket claimer:
-        
-        Your token is being used.
-        Follow guides on how to properly utilise this bot and avoid a discord ban.
-        By running this software, you are agreeing to the terms of service found in the Zeppelin Software discord server.
-        ''')
-    else:
-        print("Key could not be validated.")
-        time.sleep(5)
-        exit()
-Authorisation - requires google sheets api keys
+
 
 @client.event
 async def on_message(message):
@@ -73,8 +56,8 @@ async def on_message(message):
     content2 = Content[6:]
     #print("Message detected")
     if message.content.startswith("token"):
-        with open(file,"r+") as f:
-            json_obj = json.load(f)
+        with open(fi,"r+") as fi:
+            json_obj = json.load(fi)
             json2 = json_obj["settings"]
             json3 = json2[0]
             Token = content2
@@ -82,24 +65,11 @@ async def on_message(message):
             f.close()
             write(json_obj,file)
     if message.content.startswith("load"):
-        trigger = 1
-    if message.content.startswith("reset"):
-        with open("settings.json","w") as f:
-            f.write('''
-                {
-                    "settings": [
-                    {
-                        "delay": "0",
-                        "token": "a"
-                    }
-                ]
-            }
-''')
-            f.close()            
-    channel = message.channel
-    user = message.author
-    server = client.get_guild(guildid)
-    content = message.content
+        trigger = 1        
+        channel = message.channel
+        user = message.author
+        server = client.get_guild(guildid)
+        content = message.content
     if content == "Test":
         time.sleep(delay2)
         await channel.send("Test message detected.")
@@ -114,7 +84,6 @@ async def on_guild_channel_create(channel):
     if Guild.id == serverid:
         #print("Guild detected")
         if keyword in ticketname:
-            await output.send("Channel created")
             time.sleep(delay2)
             await channel.send("{}".format(message))
 
